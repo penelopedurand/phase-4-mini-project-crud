405733f590e4dc37a4e45a2ba605628428421c34
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-06-07 17:29:28 -0400

    commit tree b8b0f8762e6f050208289d52d4f02542087e4f55

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get '/spices', to: 'spices#index'
+resources :spices, only: [:index, :create, :update, :destroy]
 end
